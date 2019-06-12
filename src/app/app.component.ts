b9d14b9e8da84b811d8287b49303d49914aaf492
adityamudgil2505 <adityamudgil2505@gmail.com>
2019-06-13 00:21:32 +0530

    Setting component file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/interval';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/takeWhile';
+import 'rxjs/add/operator/do';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Countdown-Desktop-Application';
+
+  max=1;
+  curr=0;
+  start(){
+    const interval = Observable.interval(100);
+    interval.takeWhile( _ => this.isFinished)
+            .do( i => this.curr +=0.1 )
+            .subscribe();
+  }
+  finish(){
+    this.curr=this.max;
+  }
+  reset(){
+    this.curr=0;
+  }
+
+  // Error Handling
+  get maxVal(){
+    return isNaN(this.max) || this.max<0.1 ? 0.1:this.max;
+  }
+  get currVal(){
+    return isNaN(this.curr) || this.curr<0 ? 0:this.curr;
+  }
+  get isFinished(){
+    return this.currVal>this.maxVal;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { RoundProgressComponent }  from 'angular-svg-round-progressbar';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    RoundProgressComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
